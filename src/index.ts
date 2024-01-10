80627829fda0d54c6a264c14c79e114709ed5f08
Charlie <66382380+T2ThatGuy@users.noreply.github.com>
2024-01-10 09:28:45 +0000

    chore: Expose types in index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,15 @@ export {
   AculabCloudClient as default,
 } from './aculab-cloud-client';
 export {AculabCloudCaller} from './aculab-cloud-caller';
+
+export type {
+  AculabCloudCall,
+  AculabCloudIncomingCall,
+  AculabCloudOutgoingCall,
+  MuteObj,
+  CallObj,
+  CallOptions,
+  MediaCallObj,
+  OnIncomingObj,
+  DisconnectedCallObj,
+} from './types';
