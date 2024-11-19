fa9f2b36fe4c2536ddbdf58cc053cef7c2e28230
CharlieS <charlie.stone@aculab.com>
2024-11-19 13:00:25 +0000

    chore: improved ts-build pipeline using rollup
    note: we need to fix the type errors surrounding WebRTC and update the build command back to && instead of || when possible

---
+++ configs/rollup.config.js
@@ -0,0 +1,22 @@
+
+import typescript from '@rollup/plugin-typescript'
+
+export default [
+    {
+        input: "src/index.ts",
+        output: [
+            {
+                file: "lib/index.mjs",
+                format: "es",
+                sourcemap: false,
+                exports: "named"
+            },
+        ],
+        plugins: [
+            typescript({
+                tsconfig: "./configs/tsconfig.esm.json",
+                sourceMap: false,
+            })
+        ]
+    }
+]
